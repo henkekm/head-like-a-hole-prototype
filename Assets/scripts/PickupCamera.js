e1f216cd43f4887bc91f0b6fe92e9b114d9e6ab9
Kevin Henke <henkekm@gmail.com>
2017-06-03 10:14:11 -0400

    First Upload of working prototype to share

---
+++ Assets/scripts/PickupCamera.js
@@ -0,0 +1,103 @@
+﻿#pragma strict
+
+var throwableCamera : GameObject;
+var cameraPosition : GameObject;
+var motorScript : CharacterMotor;
+var freelookScript : MouseLook;
+var throwVelocity = 200;
+private var cameraIsHeld : boolean = false;
+private var cameraIsPickupable : boolean = true;
+
+function Start ()
+{
+	InitializeCamera();
+}
+
+function Update ()
+{
+	ThrowCamera();
+	Footsteps();
+}
+
+function OnTriggerEnter(collision : Collider)
+{
+	Debug.Log("The player is triggering "+collision.gameObject);
+	if(collision.gameObject.name == "CameraTrigger")
+	{
+		DisableMovement();
+		EnableFreelook();
+		throwableCamera.transform.position = cameraPosition.transform.position;
+		throwableCamera.rigidbody.isKinematic = true;
+		cameraIsHeld = true;
+		
+		cameraIsPickupable = false;
+	}
+}
+
+function ThrowCamera()
+{
+    if (Input.GetButtonDown("Fire1") && cameraIsHeld == true)
+    {
+		throwableCamera.rigidbody.isKinematic = false;
+		throwableCamera.rigidbody.freezeRotation = false;
+		
+ 	    throwableCamera.rigidbody.AddForce(throwableCamera.transform.forward * throwVelocity);
+ 	    
+ 	    cameraIsHeld = false;
+ 	    
+ 	    EnableMovement();
+ 	    DisableFreelook();
+// 	    cameraIsPickupable = true;
+    }
+}
+
+function InitializeCamera()
+{
+	DisableFreelook();
+}
+
+function DisableMovement()
+{
+	motorScript = GetComponent(CharacterMotor); 
+	motorScript.enabled = false;	
+}
+
+function EnableMovement()
+{
+//	motorScript = GetComponent(CharacterMotor); 
+	motorScript.enabled = true;
+}
+
+function DisableFreelook()
+{
+	freelookScript = throwableCamera.GetComponent(MouseLook);
+	freelookScript.enabled = false;
+}
+
+function EnableFreelook()
+{
+	freelookScript = throwableCamera.GetComponent(MouseLook);
+	freelookScript.enabled = true;
+}
+
+function Footsteps()
+{
+	if (cameraIsHeld == false)
+	{
+		if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+		{
+			if(!audio.isPlaying)
+			{
+	        	audio.Play();
+	        }
+	    }
+	    else
+	    {
+	    	audio.Stop();
+	    }
+	}
+	else
+	{
+		audio.Stop();
+	}
+}
---
+++ Assets/scripts/ThrowAnything.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var throwable : GameObject;
+var held : GameObject;
+
+
+function Start () {
+
+}
+
+function Update () {
+	ThrowRigidbody();
+}
+
+function ThrowRigidbody()
+{
+	var speed = 200;
+ 
+    if (Input.GetButtonDown("Fire1"))
+    {
+    	throwable.transform.position = held.transform.position;
+ 	    throwable.rigidbody.AddForce(held.transform.forward * speed);
+    }
+}
